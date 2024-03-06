c25ec9b29d7226acd0febbecb3eefc22051221e1
fran6 <igwefran6@gmail.com>
2024-03-06 21:37:18 +0100

    Modified index.js... Added a new funtionality that allows the menu dialog to move out to other sides

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,36 @@ let menuBtn = document.querySelector(".menu-btn");
 let menuDialog = document.querySelector(".menu-dialog");
 let closeBtn = document.querySelector("#close-btn");
 let menuOpened = false;
+let moveRight = "-400%, 400%";
+let moveLeft = "400%, 400%";
+let movement;
+let moved = false;
 
 menuBtn.addEventListener("click", () => {
     if (!menuOpened) {
-        menuDialog.style.transform = "translate(50%, 50%)";
+        menuDialog.style.transform = `translate(50%, 50%)`;
         menuOpened = true;
     } else {
-        menuDialog.style.transform = "translate(400%, 400%)";
+        menuDialog.style.transform = `translate(${movement})`;
         menuOpened = false;
+        if (!moved) {
+            movement = moveLeft;
+            moved = true;
+        } else {
+            movement = moveRight;
+            moved = false;
+        }
     }
 });
 
 closeBtn.addEventListener("click", () => {
-    menuDialog.style.transform = "translate(200%, 200%)";
+    menuDialog.style.transform = `translate(${movement})`;
     menuOpened = false;
+    if (!moved) {
+        movement = moveLeft;
+        moved = true;
+    } else {
+        movement = moveRight;
+        moved = false;
+    }
 });
