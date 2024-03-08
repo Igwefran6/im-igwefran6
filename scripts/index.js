73b3146ede3d0883812aba45128c4725c0980be3
fran6 <igwefran6@gmail.com>
2024-03-08 13:36:30 +0100

    Added a project section... 70% done with it

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 let menuBtn = document.querySelector(".menu-btn");
+let sharePage = document.querySelector(".share-page");
 let menuDialog = document.querySelector(".menu-dialog");
 let closeBtn = document.querySelector("#close-btn");
 let scrollToPage = document.querySelectorAll(".scroll-to ul li");
@@ -32,15 +33,24 @@ function activateBtn(item, myItem) {
     });
 }
 
+
+sharePage.addEventListener("click", ()=>{
+  if(sharePage.classList.contains("rotate90deg")){
+    console.log("works");
+    rotate0deg(sharePage)
+  }else{
+    rotate90deg(sharePage)
+  }
+})
 menuBtn.addEventListener("click", () => {
-  menuBtn.classList.add("rotate90deg")
+  rotate90deg(menuBtn)
     if (!menuOpened) {
         menuDialog.style.transform = `translate(50%, 50%)`;
         menuOpened = true;
     } else {
         menuDialog.style.transform = `translate(${movement})`;
         menuOpened = false;
-        menuBtn.classList.remove("rotate90deg")
+        rotate0deg(menuBtn)
         if (!moved) {
             movement = moveLeft;
             moved = true;
@@ -52,7 +62,7 @@ menuBtn.addEventListener("click", () => {
 });
 
 closeBtn.addEventListener("click", () => {
-  menuBtn.classList.remove("rotate90deg")
+  rotate0deg(menuBtn)
     menuDialog.style.transform = `translate(${movement})`;
     menuOpened = false;
     if (!moved) {
@@ -63,3 +73,10 @@ closeBtn.addEventListener("click", () => {
         moved = false;
     }
 });
+
+function rotate90deg(item) {
+  item.classList.add("rotate90deg")
+}
+function rotate0deg(item) {
+  item.classList.remove("rotate90deg")
+}
