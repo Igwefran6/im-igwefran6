f9eb6bed462492dc7c066b55a86151eaee216bbe
fran6 <igwefran6@gmail.com>
2024-03-12 22:05:42 +0100

    Added a dark mode toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,34 @@
 let menuBtn = document.querySelector(".menu-btn");
-let sharePage = document.querySelector(".share-page");
+let toggleMode = document.querySelector(".toggle-mode");
 let menuDialog = document.querySelector(".menu-dialog");
 let closeBtn = document.querySelector("#close-btn");
 let scrollToPage = document.querySelectorAll(".scroll-to ul li");
 let menuDialogList = document.querySelectorAll(".menu-dialog ul li");
 let desktopMenus = document.querySelectorAll(".desktop-menus li");
 let mainSections = document.querySelectorAll(".about, .projects, .contact");
+let body = document.querySelector("body");
+let preferedColor
 let menuOpened = false;
 let moveRight = "-400%, 400%";
 let moveLeft = "400%, 400%";
 let movement = moveRight;
 let moved = false;
 
-console.log(mainSections);
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Dark mode preferred
+    preferedColor = "dark"
+} else {
+    // Light mode preferred
+    preferedColor = "light"
+}
 
+function toSystemColor() {
+  body.classList.remove("dark")
+  body.classList.remove("light")
+  body.classList.add(preferedColor)
+}
+
+toSystemColor()
 const inputElement = document.querySelectorAll("input");
 
 inputElement.forEach(element => {
@@ -45,30 +60,29 @@ function activateBtn(item, myItem) {
             event.classList.remove("active-button");
         });
         button.target.classList.add("active-button");
-        scrollToTarget(button)
+        scrollToTarget(button);
     });
 }
 
 function scrollToTarget(button) {
-  let buttonText = button.target.textContent.trim()
-  console.log(buttonText);
-  if (buttonText === "About") {
-    mainSections[0].scrollIntoView({ behavior: 'smooth' });
-  }
-  if (buttonText === "Projects") {
-    mainSections[1].scrollIntoView({ behavior: 'smooth' });
-  }
-  if (buttonText === "Contact") {
-    mainSections[2].scrollIntoView({ behavior: 'smooth' });
-  }
+    let buttonText = button.target.textContent.trim();
+    console.log(buttonText);
+    if (buttonText === "About") {
+        mainSections[0].scrollIntoView({ behavior: "smooth" });
+    }
+    if (buttonText === "Projects") {
+        mainSections[1].scrollIntoView({ behavior: "smooth" });
+    }
+    if (buttonText === "Contact") {
+        mainSections[2].scrollIntoView({ behavior: "smooth" });
+    }
 }
 
-sharePage.addEventListener("click", () => {
-    if (sharePage.classList.contains("rotate90deg")) {
-        console.log("works");
-        rotate0deg(sharePage);
+toggleMode.addEventListener("click", () => {
+    if (body.classList.contains("light")) {
+        darkMode(toggleMode)
     } else {
-        rotate90deg(sharePage);
+        lightMode(toggleMode)
     }
 });
 menuBtn.addEventListener("click", () => {
@@ -108,6 +122,26 @@ function rotate90deg(item) {
 }
 function rotate0deg(item) {
     item.classList.remove("rotate90deg");
+}
+
+function darkMode(item) {
+    item.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24
+    24" height="1.5em" width="1.5em" ><title>white-balance-sunny</title><path d="M3.55 19.09L4.96 20.5L6.76
+    18.71L5.34 17.29M12 6C8.69 6 6 8.69 6 12S8.69 18 12 18 18 15.31 18 12C18
+    8.68 15.31 6 12 6M20 13H23V11H20M17.24 18.71L19.04 20.5L20.45 19.09L18.66
+    17.29M20.45 5L19.04 3.6L17.24 5.39L18.66 6.81M13 1H11V4H13M6.76 5.39L4.96
+    3.6L3.55 5L5.34 6.81L6.76 5.39M1 13H4V11H1M13 20H11V23H13" /></svg>`;
+    body.classList.remove("light")
+    body.classList.add("dark")
+}
+function lightMode(item) {
+    item.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24
+                   24"
+                  height="1.5em"                         width="1.5em" 
+                   ><title>moon-waning-crescent</title><path d="M2 12A10 10 0
+                   0 0 15 21.54A10 10 0 0 1 15 2.46A10 10 0 0 0 2 12Z" /></svg>`;
+                   body.classList.remove("dark")
+                   body.classList.add("light")
 }
 
 const msg = document.getElementById("message");
@@ -169,5 +203,3 @@ let closeFailureBox = (window.onload = function () {
             );
         });
 });
-
-
